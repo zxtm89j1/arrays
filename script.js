04916750442840a80d6ff62ac5930b99d74c18ce
Spencer Bohol <zxtm89j1@gmail.com>
2022-09-04 18:28:05 +0800

    the at method

--- script.js
+++ script.js
@@ -74,31 +74,39 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+// getting last array element
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr.length - 1);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
 
-// Slice method
-console.log(arr.slice(2));
-console.log(arr.slice(2, 4));
-console.log(arr.slice(-2));
-console.log(arr.slice(1, -2));
+// let arr = ['a', 'b', 'c', 'd', 'e'];
 
-// Splice  -- mutates the array
-// console.log(arr.splice(2));
-arr.splice(-1);
-console.log(arr);
-arr.splice(1, 2);
-console.log(arr);
+// // Slice method
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(1, -2));
 
-// Reverse -- mutates the array
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-console.log(arr2);
+// // Splice  -- mutates the array
+// // console.log(arr.splice(2));
+// arr.splice(-1);
+// console.log(arr);
+// arr.splice(1, 2);
+// console.log(arr);
 
-// concat
-const letters = arr.concat(arr2);
-console.log(letters);
+// // Reverse -- mutates the array
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+// console.log(arr2);
 
-// join
-// console.log(letters.join(' - '));
-console.log(typeof letters.join(' - '));
+// // concat
+// const letters = arr.concat(arr2);
+// console.log(letters);
+
+// // join
+// // console.log(letters.join(' - '));
+// console.log(typeof letters.join(' - '));
