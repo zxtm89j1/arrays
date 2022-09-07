4662d242c0fbe6cb5af228b2cf72df5565cdac51
Spencer Bohol <zxtm89j1@gmail.com>
2022-09-07 16:23:43 +0800

    the map method

--- script.js
+++ script.js
@@ -96,6 +96,29 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+const eurToUsd = 1.1;
+const movementsUSD = movements.map(mov => {
+  return mov * eurToUsd;
+});
+
+console.log(movements);
+console.log(movementsUSD);
+
+// using a loop (comparing map and looping)
+const movementsUSDfor = [];
+for (const mov of movements) {
+  movementsUSDfor.push(mov * eurToUsd);
+}
+
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map((mov, i) => {
+  return `Movement ${i + 1}: You ${
+    mov > 0 ? 'deposited' : 'withdrew'
+  } ${Math.abs(mov)}`;
+});
+
+console.log(movementsDescriptions);
 
 // currencies.forEach((value, key, map) => {
 //   console.log(`${key}: ${value}`);
